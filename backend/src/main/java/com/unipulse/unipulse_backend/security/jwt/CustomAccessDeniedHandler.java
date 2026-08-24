368b47fae9ae366306c3becd3c7a6cac99a0c88f
AmandiArnagala <amandi20030520@gmail.com>
2026-08-24 11:16:21 +0530

    feat(security): implement custom 401 & 403 HTTP security response handlers

---
+++ backend/src/main/java/com/unipulse/unipulse_backend/security/jwt/CustomAccessDeniedHandler.java
@@ -0,0 +1,40 @@
+package com.unipulse.unipulse_backend.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("Access denied error: {}", accessDeniedException.getMessage());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_FORBIDDEN);
+        body.put("error", "Forbidden");
+        body.put("message", "You do not have required permissions to access this resource.");
+        body.put("path", request.getServletPath());
+        body.put("timestamp", OffsetDateTime.now().toString());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+}
